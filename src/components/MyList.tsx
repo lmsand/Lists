0a80c895a4670bec65c8b0e2cf1cbbedef581c8f
lmsand <lmsand99@icloud.com>
2024-03-29 14:10:24 -0500

    Fetch data from REST API

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,13 +1,31 @@
 import character from "../data/character.json";
 import CharacterListItem from "./CharacterListItem";
-import { FlatList, Text } from "react-native";
+import { ActivityIndicator, FlatList, Text } from "react-native";
+import { useState, useEffect } from "react";
 
 const MyList = () => {
-  // return <CharacterListItem character={character.results[0]} />;
+  const [loading, setLoading] = useState(false)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setLoading(true)
+      const response = await fetch('https://rickandmortyapi.com/api/character')
+      const responseJson = await response.json()
+                                                    // console.log(JSON.stringify(responseJson, null , 2)) user friendly view of the json in the console
+      setItems(responseJson.results)
+      setLoading(false) // if results stop loading
+    }
+    fetchItems()
+  }, [])
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
 
   return (
     <FlatList
-      data={character.results}
+      data={items}
       renderItem={({ item }) => (
         <CharacterListItem character={item} />
       )}

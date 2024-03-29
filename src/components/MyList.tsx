eb30a37e6d7c75692299f26b8820fd81973f9198
lmsand <lmsand99@icloud.com>
2024-03-29 15:14:15 -0500

    basic pagination

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,35 +1,47 @@
 import character from "../data/character.json";
 import CharacterListItem from "./CharacterListItem";
-import { ActivityIndicator, FlatList, Text } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import { useState, useEffect } from "react";
 
 const MyList = () => {
-  const [loading, setLoading] = useState(false)
-  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+  const [nextPage, setNextPage] = useState("https://rickandmortyapi.com/api/character");
 
-  useEffect(() => {
-    const fetchItems = async () => {
-      setLoading(true)
-      const response = await fetch('https://rickandmortyapi.com/api/character')
-      const responseJson = await response.json()
-                                                    // console.log(JSON.stringify(responseJson, null , 2)) user friendly view of the json in the console
-      setItems(responseJson.results)
-      setLoading(false) // if results stop loading
-    }
-    fetchItems()
-  }, [])
+  const fetchNextPage = async () => {
+    setLoading(true);
+    const response = await fetch(nextPage);
+    const responseJson = await response.json();
+    // console.log(JSON.stringify(responseJson, null , 2)) user friendly view of the json in the console
 
-  if (loading) {
-    return <ActivityIndicator />
+    setItems((existingItems) => {
+      return [...existingItems, ...responseJson.results]
+    });
+    setNextPage(responseJson.info.next);
+
+    setLoading(false);
   }
+
+  useEffect(() => { // component mounted
+    fetchNextPage();
+  }, []);
+
 
   return (
     <FlatList
       data={items}
-      renderItem={({ item }) => (
-        <CharacterListItem character={item} />
-      )}
+      renderItem={({ item }) => <CharacterListItem character={item} />}
       contentContainerStyle={{ gap: 10 }}
+      ListFooterComponent={() => (
+        <View>
+           {/* will always be below existing items */}
+          {loading && <ActivityIndicator />}
+
+          <Text style={{ alignSelf: "center", fontSize: 20, color: "blue" }} onPress={fetchNextPage}>
+          Load more
+        </Text>
+        </View>
+      )}
     />
   );
 };
